b356ebdf12bb016d660b8acb16d525297bb00e42
Andrew Curd <andrewc2695@gmail.com>
2021-03-05 14:56:26 -0800

    cleaned up some file

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// import firebase from "firebase";
+
+// var firebaseConfig = {
+//     apiKey: "AIzaSyD4Hff8rEXgLvxLNnZP3uIOryaE6amaTvA",
+//     authDomain: "hit-the-target-8bc75.firebaseapp.com",
+//     databaseURL: "https://hit-the-target-8bc75-default-rtdb.firebaseio.com",
+//     projectId: "hit-the-target-8bc75",
+//     storageBucket: "hit-the-target-8bc75.appspot.com",
+//     messagingSenderId: "1045996349397",
+//     appId: "1:1045996349397:web:a5785f5a85f9b18a2bff5c",
+//     measurementId: "G-BR49HMWVC4"
+// };
+
+// const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+// const db = firebaseApp.firestore();
+// export default db;
